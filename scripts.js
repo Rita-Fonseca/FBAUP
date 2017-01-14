884dc9569550b8503b84206ea192d167bf388ee3
Rita-Fonseca <rita.fonseca@live.com.pt>
2017-01-14 01:40:09 +0000

    versão_7

--- scripts.js
+++ scripts.js
@@ -33,3 +33,26 @@ function extra() {
 
 
 })
+
+$("#lista_eventos li a.linkcategoria").on('click', function(event){
+	var categor = $(this).data("filtrar");
+
+
+
+	$("#eventos").find(".evento .imagem a").fadeTo( "fast" , 0.06);
+	$("#eventos").find("[data-filtro='" + categor + "']").fadeTo( "fast" , 1);
+
+	event.preventDefault();
+
+});
+
+$("#lista_eventos li a.todos").on('click', function(event){
+
+
+
+	$("#eventos").find(".evento .imagem a").fadeTo( "fast" , 1);
+
+
+	event.preventDefault();
+
+});
